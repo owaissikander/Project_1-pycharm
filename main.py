9415e704a55f963b6b2e5902782ab9b3745d26ad
owaissikander <154621974+owaissikander@users.noreply.github.com>
2024-06-25 14:55:21 +0500

    submit

---
+++ main.py
@@ -0,0 +1,51 @@
+# myname=[45,67,88,88,99]
+# print(myname[0:3])
+# myname[0:3]=['N','S']
+# print(myname[0])
+# myname[0]="owais"
+# print(myname[0])
+# print(len(myname))
+# print(type(myname))
+
+# name=input("enter your name ");
+#
+# age=int(input("enter your age"));
+# print(f"your name is {name} and your age is {age}");
+# list=[1,2,3,4,5]
+# num=input("enter your name");
+# list.append(num)
+# print(list);
+
+# percentage=float(input("enter yyypur percentage"));
+# if(80>=percentage):
+#     print("you are Passed")
+# elif(70>percentage):
+#     print("You are passed")
+# elif(60>percentage):
+#     print("your are passed too")
+# else:
+#     print("fail")
+
+myname=[45,67,88,89,99]
+print(myname[0:4])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ owais.py
@@ -0,0 +1,3 @@
+myname=[45,67,88,88,99]
+print(myname[0:3])
+myname[0:3]=['N','S']
